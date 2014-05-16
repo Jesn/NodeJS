028ace8564200b95d59d2b95bc58de5b5c48dc3e
jesn <jesn2013@hotmail.com>
2014-05-17 00:40:40 +0800

    2014-5-16 -23-18

--- FirstPackage/lib/argv.js
+++ FirstPackage/lib/argv.js
@@ -1,3 +1,25 @@
 /**
  * Created by Administrator on 2014/5/17.
  */
+
+//console.log(process.argv);
+
+//process.stdout.resume();
+//process.stdout.write('read from console:'+data2.toString());
+
+/*
+function doSomething(args,callback){
+    somethingComplicated(args);
+    process.nextTick(callback);
+}
+
+doSomething(function onEnd(){
+    compute();
+});
+ */
+console.log(123);
+console.log('hello%param');
+console.log('hello %d',2014);
+
+//向标准错误流输出当前的调用栈
+//console.trace();
